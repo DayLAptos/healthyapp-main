10e97ced1af8fdedfc8bcce009472d5a26928f95
DayLAptos <seletkovslava@yandex.ru>
2025-01-12 19:49:35 +0500

    a

--- final.py
+++ final.py
@@ -1,5 +1,5 @@
-from PyQt5.QtWidgets import *
-from PyQt5.QtCore import *
+from PyQt5.QtWidgets import QPushButton, QVBoxLayout, QLabel, QWidget, QApplication
+from PyQt5.QtCore import QTime, QTimer
 from instr import *
 
 
